d0eae299b147ee75f3ea1b8a0e0abb6880e36b3e
Anmol <mranmolsingh101@gmail.com>
2021-03-17 00:02:52 +0530

    C++

--- P0iNter.cpp
+++ P0iNter.cpp
@@ -6,12 +6,18 @@ int main() {
   int a=3;
   int* b;
   b=&a;
+  
 
 
-  cout<<"Address of A" <<b<<endl;
-  cout<<"Address of A" <<&a<<endl;
+  cout<<"Address of A " <<b<<endl;
+  cout<<"Address of A " <<&a<<endl;
 
-  cout<<"the value of Address" <<*b<<endl;
-
+  cout<<"The value of Address " <<*b<<endl;
+  
+  int** c=&b;
+  cout<< "Address of b "<<c<<endl;
+  cout<< "Address of b " << b<<endl;
+  cout<<" The value of Address of b " <<*c<<endl;
+  cout<<"The value of Address of value_at(value_at c) " << **c<<endl; 
   return 0;
   }
